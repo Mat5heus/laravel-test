e57783534a248592db798850dc4e0024dbe303e6
Mat5heus <matheus.santos28@alunos.fho.edu.br>
2021-01-31 19:45:39 -0300

    3 formas  de agrupar as rotas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\TesteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,10 +18,50 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/work', function () {
-    return redirect()->route('rota.test');
-});
+
 
 Route::get("/test", function () {
     return "<h1>I'm just testing some things</h1>";
 })->name('rota.test');
+/*
+*********************************************************
+        Diferentes formas de criar rotas
+*********************************************************
+
+\\\\\ Forma 1 \\\\\\\:
+Route::get('/work', function () {
+    return redirect()->route('rota.test');
+})->middleware([]);
+
+\\\\\ Forma 2 \\\\\\\:
+Route::middleware([])->group(function () {
+    Route::prefix("admin")->group(function () {
+        Route::namespace("Admin")->group(function () {
+            Route::name("admin.")->group(function () {
+                Route::get("/dashboard/{user}", [TesteController::class,'teste'])->name('dashboard');
+                Route::get("/financeiro", [TesteController::class, 'teste'])->name("financeiro");
+                Route::get("/shop", [TesteController::class, 'teste'])->name("shop");
+                Route::get("/", function () {
+                    return redirect()->route('admin.dashboard');
+                })->name('home');
+            });
+        });
+    });
+});
+
+////// Forma 2 \\\\\\\:
+Route::group([
+    'middleware' => [],
+    'prefix' => "admin",
+    'name' => "admin."
+], function() {
+    Route::get("/dashboard/{user}", [TesteController::class,'teste'])->name('dashboard');
+    Route::get("/financeiro", [TesteController::class, 'teste'])->name("financeiro");
+    Route::get("/shop", [TesteController::class, 'teste'])->name("shop");
+    Route::get("/", function () {
+        return redirect()->route('admin.dashboard');
+    })->name('home');
+});
+
+*/
+
